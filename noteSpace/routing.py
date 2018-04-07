440f2648e55a09ef613594bf7a21e0244eade71c
FIRST_NAME LAST_NAME <johnxue88@yahoo.com>
2018-04-07 19:29:19 -0400

    added pytest to req and added files for django channels in workspace

--- noteSpace/routing.py
+++ noteSpace/routing.py
@@ -2,11 +2,13 @@ from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
+import workspace.routing
 
 application = ProtocolTypeRouter({
     "websocket": AuthMiddlewareStack(
         URLRouter([
             # Put paths here
+            workspace.routing.websocket_urlpatterns,
         ])
     ),
 })
