9a592a11b3d4b87e08ec47a8c8c13e19ab3dc306
Taranvir Waraich <taranvir@me.com>
2018-04-07 15:59:14 -0400

    Added the authentication urls

--- noteSpace/urls.py
+++ noteSpace/urls.py
@@ -18,4 +18,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', include('rest_auth.urls')),
+    path('api/auth/registration', include('rest_auth.registration.urls')),
 ]
