a9123bd08171653ff4c80e83872c6dd6260934f6
Gianfranco Leto <John@Gianfrancos-MacBook-Pro.local>
2018-04-08 06:09:50 -0400

    added card serializer to workspace serializer in serializers and swapped the serializers... serializer.

--- workspace/serializers.py
+++ workspace/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from .models import Workspace, Card
 
-#Workspace Serializer
-class WorkspaceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Workspace
-        fields = ('owner', 'title','members', 'id')
 #Card Serializer
 class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
         fields = ('workspace', 'front', 'back', 'last_edit', 'locked', 'id')
+
+#Workspace Serializer
+class WorkspaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workspace
+        fields = ('owner', 'title','members', 'id')
+    cards = CardSerializer(many=True, read_only=True)
